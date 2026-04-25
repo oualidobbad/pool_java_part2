b8eab1ea8aecd5e7f56a5b0f5ce0acf39415a839
Oualid Obbad <oobbad@c2r3p5.1337.ma>
2026-04-25 17:23:23 +0100

    i create 90 % of ex02 last thing is test of delete with implementation of both

--- Module06/ex02/Tests/src/test/java/fr/s42/repositories/ProductsReposutoryJdbcImplTest.java
+++ Module06/ex02/Tests/src/test/java/fr/s42/repositories/ProductsReposutoryJdbcImplTest.java
@@ -1,7 +1,10 @@
 package fr.s42.repositories;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.beans.Transient;
+import java.lang.annotation.Target;
 import java.math.BigDecimal;
 
 import javax.sql.DataSource;
@@ -12,18 +15,24 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import java.util.List;
+import java.util.Optional;
+
 import fr.s42.models.Product;
 
 public class ProductsReposutoryJdbcImplTest {
 	private  DataSource dataSource;
 	private  ProductsRepositoryJdbcImpl pJdbcImpl;
+
 	private final List<Product> EXPECTED_FIND_ALL_PRODUCTS = List.of(
     new Product(1L, "Wireless Mouse", new BigDecimal("25.99")),
     new Product(2L, "Mechanical Keyboard", new BigDecimal("79.50")),
     new Product(3L, "USB-C Hub", new BigDecimal("45.00")),
     new Product(4L, "Gaming Monitor", new BigDecimal("299.99")),
     new Product(5L, "Noise Cancelling Headphones", new BigDecimal("150.25"))
-);
+	);
+
+	private final Product EXPECTED_FIND_BY_ID_PRODUCT = new Product(1L, "Wireless Mouse", new BigDecimal("25.99"));
+	private final Product EXPECTED_UPDATED_PRODUCT = new Product(1L, "Wireless Mouse Pro", new BigDecimal("43.66"));
 
 	@BeforeEach
 	void init (){
@@ -35,8 +44,47 @@ public class ProductsReposutoryJdbcImplTest {
 
 		pJdbcImpl = new ProductsRepositoryJdbcImpl(dataSource);
 	}
+
+	//test findAll
 	@Test
 	void testFindAll(){
 		assertEquals(EXPECTED_FIND_ALL_PRODUCTS, pJdbcImpl.findAll());
+	}
+
+	// test findById
+	@Test
+	void testFindByIdTrueId()
+	{
+		Optional<Product> pr = pJdbcImpl.findById(1L);
+		assertTrue(pr.isPresent());
+		assertEquals(EXPECTED_FIND_BY_ID_PRODUCT, pr.get());
+	}
+
+	@Test
+	void testFindByIdFakeId()
+	{
+		Optional<Product> pr = pJdbcImpl.findById(99L);
+		assertTrue(pr.isEmpty());
+	}
+
+	// test update
+	@Test
+	void testUpdate(){
+		pJdbcImpl.update(EXPECTED_UPDATED_PRODUCT);
+		Optional<Product> pr = pJdbcImpl.findById(1L);
+		assertTrue(pr.isPresent());
+		assertEquals(EXPECTED_UPDATED_PRODUCT, pr.get());
+
+	}
+
+	@Test 
+	void testSave(){
+		Product pr = new Product(null, "Google pixel 7", new BigDecimal("9999.99"));
+
+		pJdbcImpl.save(pr);
+		assertTrue(pr.getId() != null);
+		Optional<Product> productFinded = pJdbcImpl.findById(pr.getId());
+		assertTrue(productFinded.isPresent());
+		assertEquals(pr, productFinded.get());
 	}
 }
